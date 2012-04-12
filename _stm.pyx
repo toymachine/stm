cbf3dd44fc256e5c8190fedd095261aaf8953046
Henk Punt <henkpunt@gmail.com>
2012-04-12 17:05:54 +0200

    bla

--- _stm.pyx
+++ _stm.pyx
@@ -1,10 +1,23 @@
-cdef class BlaatAap2:
+cdef extern from "stdlib.h":
+     void free(void* ptr)
+     void* malloc(size_t size)
+     void* realloc(void* ptr, size_t size)
+
+cdef class Persistent:
+     cdef blaat(self):
+          pass
+
+cdef class PersistentVector(Persistent):
      cdef int shift
      cdef int cnt
-     cdef object bla
+     cdef Persistent root #array!
+     cdef int root_length
+     cdef Persistent tail #array!
+     cdef int tail_length
 
-     cdef sum(self):
-         return 10 + 20
+     cdef cons(self, Persistent val):
+          if self.tail_length < 32:
+             pass
 
 def test():
-    print sizeof(BlaatAap2)
+    print sizeof(PersistentVector)
